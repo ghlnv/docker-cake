3f706462ab746f833f691cc637bc9b742f7f126f
Giordano Luperni Nogales <ghlnv@yahoo.com>
2026-04-01 16:36:44 -0300

    Melhoria de código; Container mysql 8.4;

--- docker_create_mysql84.sh
+++ docker_create_mysql84.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# File: /docker_create_mysql8.sh
-# Created: Friday, 20th September 2024 7:15:56 pm
+# File: /docker_create_mysql84.sh
+# Created: Wednesday, 1st April 2026 4:17:51 pm
 # Author: Giordano LN
 
 # Carregar variáveis do .env (no mesmo diretório do script)
 export $(grep -v '^#' .env | xargs)
 
-# Rodar container MySQL 8
-docker run --name mysql8 \
-	-p 3308:3306 \
-	-v ./mysql8:/var/lib/mysql \
+# Rodar container MySQL 8.4
+docker run --name mysql84 \
+	-p 33084:3306 \
+	-v ./mysql84:/var/lib/mysql \
 	-v ./my.cnf:/etc/my.cnf \
 	-e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
-	-d mysql:8
+	-d mysql:8.4
